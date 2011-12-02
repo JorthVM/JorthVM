b892a6c53aad813a2f3b45e5805de03c8b9cbb6b
Josef Eisl <zapster@zapster.cc>
2011-12-02 14:47:20 +0100

    fixed opcode table bug

--- jvm/decode.fs
+++ jvm/decode.fs
@@ -1,26 +1,44 @@
 \ create lookuptable
 
-: unsupported s" This Opcode is not (yet) supported" type 100 throw ;
+: jvm_op_unsupported s" This Opcode is not (yet) supported" exception throw ;
 
-variable opcodetbl 
-256 cells allocate throw \ xt for opcodes 
-opcodetbl ! \ store table pointer
+\ opcode table size
+variable jvm_opcodetbl_size
+
+\ opcode table
+variable jvm_opcodetbl 
+
+\ program counter
+variable jvm_pc
 
 : jvm_set_op ( ... xt opcode - )
-cells opcodetbl + ! \ calculate opcode-addr and store xt 
+cells jvm_opcodetbl @ + ! \ calculate opcode-addr and store xt 
 ;
 
 : jvm_execute (  ... opcode - ... [result of operation] )
-cells opcodetbl + @ execute
+cells jvm_opcodetbl @ + @ execute
+;
+
+\ see the implementeation of opcode
+: jvm_opcode_see ( ... opcode - ) 
+  cells jvm_opcodetbl @ + @ xt-see
 ;
 
 : jvm_init_op_tbl ( ... - ... )
-opcodetbl 256 erase 
+\ store opcode table size
+  256 jvm_opcodetbl_size !
+\ allocate opcode table cells
+  jvm_opcodetbl_size @ cells allocate throw \ xt for opcodes 
+  jvm_opcodetbl ! \ store table pointer
+\ jvm_opcodetbl @ jvm_opcodetbl_size @ cells erase 
+\ set default handler
+  ['] jvm_op_unsupported
+  jvm_opcodetbl_size @ 0 +DO
+\ dup i . .
+  dup i jvm_set_op 
+  LOOP
+  .
 ;
-
-\ program counter
-
-variable jvm_pc
 
 : jvm_set_pc ( ... addr -- ... )
   jvm_pc ! 

78dc8a168ba47b7935180a523378bbd54da79ad5
Josef Eisl <zapster@zapster.cc>
2011-12-04 14:39:12 +0100

    improved fetch

--- src/jvm/fetch.fs
+++ src/jvm/fetch.fs
@@ -1,8 +1,6 @@
 
 : jvm_fetch_instruction ( ... -- opcode ... )
-  POSTPONE jvm_pc POSTPONE @ 
-  POSTPONE dup 1 POSTPONE literal
-  POSTPONE + POSTPONE jvm_pc 
-  POSTPONE ! POSTPONE @ 0xff POSTPONE literal 
-  POSTPONE and \ swap
+  POSTPONE jvm_pc POSTPONE @ \ load pc 
+  POSTPONE c@ \ load instruction
+  1 POSTPONE literal POSTPONE jvm_pc POSTPONE +! \ increment pc
 ; immediate

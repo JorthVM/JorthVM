7d3f4dd07129dff5dcbf614459408529ada8e799
Josef Eisl <zapster@zapster.cc>
2011-12-20 22:24:51 +0100

    frame: initial file added

--- src/jvm/frame.fs
+++ src/jvm/frame.fs
@@ -1,3 +1,13 @@
 \ vim: sw=2 ts=2 sta et
 \ this file contains the implementaion used for the creation and management 
 \ of jvm stack frames
+
+\ ========
+\ *! frame
+\ *T Method Frame
+\ ========
+
+\ *S H2
+
+
+

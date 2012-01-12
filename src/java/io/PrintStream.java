9983d25cb328d8efc8bc56b431ed7ba082f29314
Bernhard Urban <lewurm@gmail.com>
2012-01-12 22:43:31 +0100

    mini-java.lang: adding stuff java.lang.System, java.lang.PrintStream
    yep, binary too at the momemnt, since it's a bit weird to compile stuff for
    `java.lang.*' ...

---
+++ src/java/io/PrintStream.java
@@ -0,0 +1,5 @@
+package java.io;
+
+public class PrintStream {
+	public native void println(String a);
+}
---
+++ src/testfiles/StringTest1.java
@@ -0,0 +1,5 @@
+public class StringTest1 {
+	static public void main(String args[]) {
+		System.out.println("OHAI java.lang!");
+	}
+}

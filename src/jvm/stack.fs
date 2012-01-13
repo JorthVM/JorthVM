000548cfa89a05314000c83f6ca7c4a4ee45dac5
Bernhard Urban <lewurm@gmail.com>
2012-01-13 12:28:59 +0100

    license: gplv3

--- src/jvm/stack.fs
+++ src/jvm/stack.fs
@@ -1,4 +1,24 @@
 \ vim: sw=2 ts=2 sta et
+
+\ `JorthVM', a Java VM implemented in Forth
+\ 
+\ Copyright (C) 2012 Sebastian Rumpl <e0828489@student.tuwien.ac.at>
+\ Copyright (C) 2012 Josef Eisl <zapster@zapster.cc>
+\ Copyright (C) 2012 Bernhard Urban <lewurm@gmail.com>
+\ 
+\ This program is free software: you can redistribute it and/or modify
+\ it under the terms of the GNU General Public License as published by
+\ the Free Software Foundation, either version 3 of the License, or
+\ (at your option) any later version.
+\ 
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+\ 
+\ You should have received a copy of the GNU General Public License
+\ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+\ this file implements functionality that is needed to read class files
 \ this file contains the implementaion of the JVM Stack 
 \ of jvm stack frames
 
--- src/testfiles/StringTest1.java
+++ src/testfiles/StringTest1.java
@@ -1,3 +1,22 @@
+// `JorthVM', a Java VM implemented in Forth
+// 
+// Copyright (C) 2012 Sebastian Rumpl <e0828489@student.tuwien.ac.at>
+// Copyright (C) 2012 Josef Eisl <zapster@zapster.cc>
+// Copyright (C) 2012 Bernhard Urban <lewurm@gmail.com>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 public class StringTest1 {
 	static public void main(String args[]) {
 		System.out.println("OHAI java.lang!");

a1113cdcf47310eca847d6b754aefcca29f86edf
Josef Eisl <zapster@zapster.cc>
2012-01-28 02:32:58 +0100

    licenses: updated file header

--- src/docgen/contrib/cleanhtml.sh
+++ src/docgen/contrib/cleanhtml.sh
@@ -1,19 +1,18 @@
-# `JorthVM', a Java VM implemented in Forth
-# 
-# Copyright (C) 2012 Sebastian Rumpl <e0828489@student.tuwien.ac.at>
-# Copyright (C) 2012 Josef Eisl <zapster@zapster.cc>
-# Copyright (C) 2012 Bernhard Urban <lewurm@gmail.com>
-# 
-# This program is free software: you can redistribute it and/or modify
+#
+# Copyright (C) 2011-2012 Sebastian Rumpl, Josef Eisl, Bernhard Urban
+#
+# This file is part of JorthVM
+#
+# JorthVM is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # this file implements functionality that is needed to read class files
@@ -45,6 +44,4 @@ s|\(<body\)|\n<link href="css/style.css" rel="stylesheet" type="text/css" >\n<st
 #s/<\/code>/<\/code><\/div>/g
 # add header
 s|<html>|<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"\n"http://www.w3.org/TR/html4/loose.dtd">\n<html>\n<head>\n<title>'$title'</title>\n<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >\n|g
-s/<body>/\n\<\/head>\n<body>\n/g ' <$1 
-
-
+s/<body>/\n\<\/head>\n<body>\n/g ' <$1
--- src/jvm/native.fs
+++ src/jvm/native.fs
@@ -1,21 +1,20 @@
 : PrintMe.printHello|()V.static ( -- )
 
-\ `JorthVM', a Java VM implemented in Forth
-\ 
-\ Copyright (C) 2012 Sebastian Rumpl <e0828489@student.tuwien.ac.at>
-\ Copyright (C) 2012 Josef Eisl <zapster@zapster.cc>
-\ Copyright (C) 2012 Bernhard Urban <lewurm@gmail.com>
-\ 
-\ This program is free software: you can redistribute it and/or modify
+
+\ Copyright (C) 2011-2012 Sebastian Rumpl, Josef Eisl, Bernhard Urban
+
+\ This file is part of JorthVM
+
+\ JorthVM is free software: you can redistribute it and/or modify
 \ it under the terms of the GNU General Public License as published by
 \ the Free Software Foundation, either version 3 of the License, or
 \ (at your option) any later version.
-\ 
+
 \ This program is distributed in the hope that it will be useful,
 \ but WITHOUT ANY WARRANTY; without even the implied warranty of
 \ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 \ GNU General Public License for more details.
-\ 
+
 \ You should have received a copy of the GNU General Public License
 \ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 \ this file implements functionality that is needed to read class files
@@ -34,3 +33,4 @@
 : java/lang/Object.registerNatives|()V.static ( )
   
 ;
+
